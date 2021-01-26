2fdd5fe065452e8a6531f8c95cd85a6b7df281d9
David <pintodiaz.d@gmail.com>
2021-01-25 21:27:14 -0300

    Creación PageView para Populares y Metodo para reciibir los datos

--- lib/src/widgets/card_swiper_widget.dart
+++ lib/src/widgets/card_swiper_widget.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
 
-
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:peliculasapp/src/models/pelicula_model.dart';
 
-
 class CardSwiper extends StatelessWidget {
-  
   final List<Pelicula> peliculas;
 
-  CardSwiper({ @required this.peliculas });
+  CardSwiper({@required this.peliculas});
 
   @override
   Widget build(BuildContext context) {
-
     final _screenSize = MediaQuery.of(context).size;
 
     return Container(
@@ -22,7 +18,7 @@ class CardSwiper extends StatelessWidget {
         layout: SwiperLayout.STACK,
         itemWidth: _screenSize.width * 0.7,
         itemHeight: _screenSize.height * 0.6,
-        itemBuilder: (BuildContext context,int index){
+        itemBuilder: (BuildContext context, int index) {
           return ClipRRect(
             borderRadius: BorderRadius.circular(20.0),
             child: FadeInImage(
@@ -36,7 +32,6 @@ class CardSwiper extends StatelessWidget {
         //pagination: new SwiperPagination(),
         //control: new SwiperControl(),
       ),
-    );   
-    
+    );
   }
 }
