ff9159d36da84ab3de2be8795d7f808725143bcd
David <pintodiaz.d@gmail.com>
2021-02-25 16:55:31 -0300

    Modelo actores, servicio obtención del cast, Crear PageView de actores

--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
+import 'package:peliculasapp/src/models/actores_model.dart';
 import 'package:peliculasapp/src/models/pelicula_model.dart';
 import 'dart:convert';
 import 'dart:async';
@@ -65,5 +66,19 @@ class PeliculasProvider {
 
     _cargando = false;
     return resp;
+  }
+
+  Future<List<Actor>> getCast(String peliId) async {
+    final url = Uri.https(_url, '3/$peliId/credits', {
+      'api_key': _apikey,
+      'language': _language,
+    });
+
+    final resp = await http.get(url);
+    final decodedData = json.decode(resp.body);
+
+    final cast = new Cast.fromJsonList(decodedData['cast']);
+
+    return cast.actores;
   }
 }
